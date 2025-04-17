483dc9d4dbb521bb49f07d019feea1b5327d2967
Abiodun Adedewe <adewaleabiodun2018@gmail.com>
2025-04-17 07:20:20 +0100

    Linked the html to css correctly.

--- alert copy.js
+++
@@ -1,8 +0,0 @@
-const popup = document.getElementById('popup');
-const closeButtons = document.querySelectorAll('.close-btn');
-
-closeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        popup.style.display = 'none';
-    });
-});
